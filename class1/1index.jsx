c2572d74e87f6bbc61a1760f018202042ec6f8d4
zjs1qw <1301338853@qq.com>
2023-03-02 14:39:13 +0800

    first commit

---
+++ class1/1index.jsx
@@ -0,0 +1,15 @@
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <h1>123</h1>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("app")
+)
